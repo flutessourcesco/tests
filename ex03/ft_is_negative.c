dc17ad0865746303f021b5fa027e6cb691d06228
flutes-sources <30963486+flutes-sources@users.noreply.github.com>
2017-08-14 16:53:41 +0200

    Update ft_is_negative.c

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -9,8 +9,12 @@
 /*   Updated: 2017/04/06 16:48:11 by aezzeddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void	ft_putchar(char c);
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);	
+}
 
 void	ft_is_negative(int n)
 {
