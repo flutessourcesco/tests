7fb6f8c28554250e52fe97a86b482b7e61cefe1d
flutes-sources <30963486+flutes-sources@users.noreply.github.com>
2017-08-14 16:06:38 +0200

    Update ft_print_alphabet.c

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -9,8 +9,12 @@
 /*   Updated: 2017/04/06 16:21:30 by aezzeddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void	ft_putchar(char c);
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);	
+}
 
 void	ft_print_alphabet(void)
 {
