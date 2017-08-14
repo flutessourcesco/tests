0899c2987b20d2a06ba2c6a3d9463bd963f5a6c9
flutes-sources <30963486+flutes-sources@users.noreply.github.com>
2017-08-14 16:56:17 +0200

    Update ft_putnbr.c

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -9,8 +9,12 @@
 /*   Updated: 2017/04/06 17:06:56 by aezzeddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
 
-void	ft_putchar(char c);
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);	
+}
 
 void	ft_putnbr(int nb)
 {
